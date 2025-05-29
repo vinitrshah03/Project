367555387f799d6d54c4934ba27583dd86f73b9a
Vinit Shah <130815103+vinitrshah03@users.noreply.github.com>
2025-05-29 14:42:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 ''' This file contains the configuration settings for the application. This is supposed to be kept secret and not shared publicly. '''
 
 # ============= Google OAuth2 Configuration =============
-GOOGLE_CLIENT_ID = "1003437659662-3b45kgsvvtn7pi0bsmdmkip3i8lgb5pc.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_CLIENT_ID = "<your_google_client_id>"
+GOOGLE_CLIENT_SECRET = "<your_google_client_secret>"
 GOOGLE_REDIRECT_URI = "http://localhost:5000/google/callback"
 
 # ========== SHODAN API CONFIGURATION ==========
-SHODAN_API_KEY = "XXq5hAOwWMaATCx5gBcASQC0j1yvNu3N"
+SHODAN_API_KEY = "<your_api_key>"
 
 # ========== MYSQL DATABASE CONFIGURATION ==========
 MYSQL_HOST = "localhost"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "123456789"
+MYSQL_USER = "<your_db_username>"
+MYSQL_PASSWORD = "<your_db_password>"
 MYSQL_DATABASE = "user_auth"
